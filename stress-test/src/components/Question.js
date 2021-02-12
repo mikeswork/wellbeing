4b71a292c8c06f3384d8340b7b97aff61c7d0d86
Mike Richardson <mike.richardson@exponentialent.com>
2021-02-11 18:09:00 -0800

    - Added ability to reverse points per question - Added score display at the end - Question content changed

--- stress-test/src/components/Question.js
+++ stress-test/src/components/Question.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const answerMax = 4;
 
-export default function Question({ number, content, onselectCallback, checkIndex, highlight }) {
-	let radios = [];
+export default function Question({ number, content, reversePoints, onselectCallback, checkIndex, highlight }) {
+	let points = [];
+    let radios = [];
 
 	const changeRadio = (e, ans) => {
 		console.log("changeRadio()");
@@ -13,18 +14,24 @@ export default function Question({ number, content, onselectCallback, checkIndex
 		}
 	};
 
-	for (let i = 0; i <= answerMax; i++) {
+    for (let i = 0; i <= answerMax; i++) {
+        points.push(i);
+    }
+
+    if (reversePoints) points.reverse();
+
+	points.forEach(pnt => {
 		radios.push(
 			<input
-				key={i}
+				key={pnt}
 				type="radio"
 				name={`answer-${number}`}
-				value={i}
-				checked={checkIndex === i}
-				onChange={(e) => changeRadio(e, i)}
+				value={pnt}
+				checked={checkIndex === pnt}
+				onChange={(e) => changeRadio(e, pnt)}
 			/>
 		);
-	}
+	})
 
 	const currStyle = highlight ? { color: "red" } : {};
 
--- stress-test/src/components/Test.js
+++ stress-test/src/components/Test.js
@@ -2,56 +2,74 @@ import React, { useState } from "react";
 import Question from "./Question";
 
 const blanks = (items) => {
-    return items.map(() => undefined);
-}
+	return items.map(() => undefined);
+};
 
 export default function Test({ instructions, questions }) {
 	const [missing, setMissing] = useState();
 	const [answers, setAnswers] = useState(blanks(questions));
+	const [score, setScore] = useState();
 
 	const selectAnswer = (num, answer) => {
 		setAnswers((currAnswers) => {
-            let newAnswers = [...currAnswers];
-            newAnswers[num] = answer;
+			let newAnswers = [...currAnswers];
+			newAnswers[num] = answer;
 
-            return newAnswers;
+			return newAnswers;
 		});
 	};
 
 	const submitAnswers = (e) => {
 		e.preventDefault();
 
-        if (answers.every(a => a !== undefined)) {
+		if (answers.every((a) => a !== undefined)) {
+			const score = answers.reduce((a, b) => a + b);
+			console.info("All complete! Score is", score);
+
 			setMissing(false);
-			setAnswers(blanks(questions));
-			console.info("All complete! Resetting.");
-        } else {
-            console.log(answers)
+            setScore(score);
+		} else {
+			console.log(answers);
 			setMissing(true);
 		}
 	};
 
+    let scoreString = "";
+    if (score !== undefined) {
+        if (score < 14) scoreString = "Scores ranging from 0-13 are considered low stress.";
+        else if (score < 27) scoreString = "Scores ranging from 14-26 are considered moderate stress.";
+        else scoreString = "Scores ranging from 27-40 are considered high perceived stress.";
+    }
+
 	return (
 		<div className="test-main">
-			<form>
-				{instructions && <h2>Instructions:</h2>}
-				<p className="test-instructions">{instructions}</p>
+			{score || score === 0 ? (
+				<>
+                    <div>Your score is: {score}</div>
+                    <div>{scoreString}</div>
+                </>
+			) : (
+				<form>
+					{instructions && <h2>Instructions:</h2>}
+					<p className="test-instructions">{instructions}</p>
 
-				<ol>
-					{answers.map((a, indx) => (
-						<Question
-							key={indx}
-							number={indx}
-							content={questions[indx].content}
-                            onselectCallback={selectAnswer}
-                            checkIndex={a}
-							highlight={missing && a === undefined}
-						/>
-					))}
-				</ol>
+					<ol>
+						{answers.map((a, indx) => (
+							<Question
+								key={indx}
+								number={indx}
+								content={questions[indx].content}
+								reversePoints={questions[indx].reversePoints}
+								onselectCallback={selectAnswer}
+								checkIndex={a}
+								highlight={missing && a === undefined}
+							/>
+						))}
+					</ol>
 
-				<input type="submit" onClick={submitAnswers} />
-			</form>
+					<input type="submit" onClick={submitAnswers} />
+				</form>
+			)}
 		</div>
 	);
 }
